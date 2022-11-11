260fe989dc80c0cf42ad2e2dee4faa9baa06f4db
Christian Perona <cperona@jaumebalmes.net>
2022-11-11 09:37:00 +0100

    Initial commit

---
+++ src/main/java/monaco/LectorDOMObjectes.java
@@ -0,0 +1,55 @@
+package monaco;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.*;
+
+public class LectorDOMObjectes {
+	
+	public void llegeixEmpleats() throws IOException {
+		try (FileInputStream reader = new FileInputStream("C:/Users/tonla/empleats_XML.xml")) {
+			
+			DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			Document document = documentBuilder.parse(reader);
+
+			// document.getDocumentElement().getNodeName() �s "empleat"
+			NodeList empleatsNodeList = document.getElementsByTagName("empleat");
+			
+			for (int i=0; i<empleatsNodeList.getLength(); i++) { // NodeList no implementa Iterable
+				Node empleatNode = empleatsNodeList.item(i);
+
+				if (empleatNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element empleatElement = (Element) empleatNode;
+					
+					empleats.add(new Employee(
+							getNodeValue("nom", empleatElement),
+							getNodeValue("cognom", empleatElement),
+							Integer.parseInt(getNodeValue("edat", empleatElement)),
+							Float.parseFloat(getNodeValue("mida", empleatElement)),
+							getNodeValue("carrec", empleatElement)
+					));
+				}
+			}	
+		} catch (IOException | ParserConfigurationException | SAXException e) {
+			throw new IOException(e);
+		}
+	}
+
+	private static String getNodeValue(String etiqueta, Element element) {
+		NodeList nodeList = element.getElementsByTagName(etiqueta).item(0).getChildNodes();
+		Node node = nodeList.item(0);
+		return node.getNodeValue();
+	}
+	
+	public void mostraEmpleats() {
+		for (Employee e : empleats)
+			System.out.println(e);
+	}
+}
+
